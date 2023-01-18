2a3ac3965dd956015d175b2d42038c0730bed417
Donkami04 <camilo.munerac@gmail.com>
2023-01-17 23:35:02 -0500

    added UPGRADE_DATE and DOWNGRADE_DATE validation

--- 01_our_microservice/customerdataapi/views.py
+++ 01_our_microservice/customerdataapi/views.py
@@ -11,8 +11,7 @@ from customerdataapi.serializers import CustomerDataSerializer
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
-from .forms import ChangeSubscription
+from django.utils import timezone
 
 
 class CustomerDataViewSet(viewsets.ModelViewSet):
@@ -31,14 +30,41 @@ def upgradeInterface(request):
     
 def upgradeLevel(request):    
     if request.method == 'POST':
+        print("XXXXXXXXX METODO POST ", request.POST)
         id = request.POST['id']
-        newSubscription = request.POST['subscription']
+        newSubs = request.POST['subscription']
+        
         customer = CustomerData.objects.get(id=id)
-        customer.data['SUBSCRIPTION'] = newSubscription
+        currentSubs = customer.data['SUBSCRIPTION']
+        
+        if currentSubs == 'premium' and newSubs == 'free' or newSubs == 'basic':
+            customer.data['DOWNGRADE_DATE'] = timezone.now()
+            
+        elif currentSubs == 'free' and newSubs == 'basic' or newSubs == 'premium':
+            customer.data['UPGRADE_DATE'] = timezone.now()
+            
+        elif currentSubs == 'basic' and newSubs == 'free':
+            customer.data['DOWNGRADE_DATE'] = timezone.now()
+            
+        elif currentSubs == 'basic' and newSubs == 'premium':
+            customer.data['UPGRADE_DATE'] = timezone.now()
+            
+        elif currentSubs == newSubs:
+            return HttpResponse(f"The user already has the Subscription {newSubs}")
+            
+        customer.data['SUBSCRIPTION'] = newSubs         
         customer.save()
+        
+        
         return HttpResponse("Succes")
     else:
         return HttpResponse("Fallo")
+    
+# def features(id):
+    
+#     customer = CustomerData.objects.get(id=id)
+#     customer.data['UPGRADE_DATE'] = timezone.now()
+#     return customer.save()
         
     
     
