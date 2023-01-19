a60bccf73310c32c80877652a06712f863a09458
Donkami04 <camilo.munerac@gmail.com>
2023-01-19 18:27:56 -0500

    Added documentation

--- 02_your_code/test/test_update_plan.py
+++ 02_your_code/test/test_update_plan.py
@@ -1,16 +1,33 @@
+"""
+Testing the upgrade/downgrade
+
+Run pytest from the root directory (02_your_code)!
+"""
+
 import requests
 import pytest
 import subprocess
 
     
 def test_if_the_get_request_works_to_the_microservice():
+    
+    """
+    This test make a get request to the microservice
+    """
 
     id = '49a6307e-c261-414d-86f5-c6004bcec8ab' 
     url = f'http://localhost:8010/api/v1/customerdata/{id}'
     response = requests.get(url)
     assert response.status_code == 200
 
-def test_upgrade_plan_customer_to_basic():
+def test_upgrade_subscription_customer_to_basic():
+    
+    """
+    This test must run after `make data` for the customerdataapi since it relies on this microservice.
+    
+    The test itself runs the `upgrade 49a6307e-c261-414d-86f5-c6004bcec8ab basic` and looks at the result.
+    """
+    
     id = '49a6307e-c261-414d-86f5-c6004bcec8ab'
         
     subprocess.run(f'cd ../02_your_code; ./cli upgrade {id} basic',
@@ -21,14 +38,19 @@ def test_upgrade_plan_customer_to_basic():
     customer = response.json()
     data = customer['data']
 
-    
     assert response.status_code == 200
     assert customer['id'] == id
     assert data['SUBSCRIPTION'] == 'basic'
     assert "DOWNGRADE_DATE" not in data
     assert "UPGRADE_DATE" in data
     
-def test_downgrade_plan_customer_to_free():
+def test_downgrade_subscription_customer_to_free():
+    
+    """
+    This test must run after `make data` for the customerdataapi since it relies on this microservice.
+
+    The test itself runs the `downgrade a237ed14-88fb-45f3-b9b1-471877dbdc60 free` and looks at the result.
+    """
     id = '49a6307e-c261-414d-86f5-c6004bcec8ab' 
     
     subprocess.run(f'cd ../02_your_code; ./cli downgrade {id} free',
@@ -44,21 +66,17 @@ def test_downgrade_plan_customer_to_free():
     assert "DOWNGRADE_DATE" in data
     assert "UPGRADE_DATE" not in data
     
+    """ Test that all the features are false"""
     for feat in data['ENABLED_FEATURES'].values():
         assert feat == False
-        
-def is_the_plan_reachable(plan):
-    avaible_plans = ['free', 'basic', 'premium']
-    if plan not in avaible_plans:
-        raise ValueError('Error code 2: the target subscription level is not reachable')
-    
-def test_test_if_plan_is_reachable():
-    with pytest.raises(ValueError):
-        is_the_plan_reachable('otroplan')
-        
+                
 def test_id_does_not_exists():
+    
+    """
+    Try with and wrong Uuid and the status code should be 404
+    """
+    
     id = 'donkami'
     url = f'http://localhost:8010/api/v1/customerdata/{id}/'
     response = requests.get(url)
     assert response.status_code == 404
-    
--- 02_your_code/update_plan.py
+++ 02_your_code/update_plan.py
@@ -1,22 +1,30 @@
-"""
-'requests' allow make any HTTP requests, also is used for connect microservices
-"""
+# requests allow make HTTP requests
 import requests
 
 from datetime import datetime
 
+
+def upgrade(id, new_subscription):
     
-def upgrade(id, new_plan):
     """_summary_
-        This function is used to upgrade the subscription of one customer.
-        Has differents validations layers
+    The upgrade function allows to improve a customer's subscription, create or actualize
+    the date of the last upgrade and replaces the date of the last downgrade if it exists.
+    Upgrade function calls the up_or_down function which validates the new subscription.
+    
     Args:
-        id (_type_): _description_
-        plan (_type_): _description_
+        id ( UUID ): Unique code to identify each customer 
+        new_subscription ( Str ) : It is the new desired subscription, only allow free, basic or premium
+        
     Raises:
-        SyntaxError: _description_
+        Error Code 1: Bad id or does not exist.
+        Error Code 2: Bad subscription option or does not exist.
+        Error Code 3: (Exception) Occurs if something goes wrong with the microservice
+            for which the request is made or unkonw error.
+        Error Code 4: If you use the upgrade function to downgrade the subscription.
+        Error Code 5: The user already has the Subscription that you are trying to update.
+        
     Returns:
-        _type_: _description_
+        Success ( Str ): Console message informing if the upgrade was successful.
     """
 
     try:
@@ -27,30 +35,52 @@ def upgrade(id, new_plan):
             return print('Error code 1: the ID does not exist or is badly formated') 
               
         data = customer['data']
-        current_plan = data["SUBSCRIPTION"]
-        result = up_or_down(current_plan, new_plan)
+        current_subscription = data["SUBSCRIPTION"]
+        result = up_or_down(current_subscription, new_subscription) # return True, False or a string message
         if result == True:
-            data['SUBSCRIPTION'] = new_plan
-            data['UPGRADE_DATE'] = datetime.now().isoformat()
+            data['SUBSCRIPTION'] = new_subscription
+            data['UPGRADE_DATE'] = datetime.now().isoformat() # .isoformat() make serializable the datetime
             try:
-                data.pop('DOWNGRADE_DATE')
+                data.pop('DOWNGRADE_DATE') # If DOWNGRADE_DATE exists then remove it, if not do nothing
             except:
                 pass
             put_request = requests.put(url, json=customer)
             if put_request.status_code == 200:
-                print("Success!!")
+                return print("Success!!")
     
         elif result == False:
             print('Error code 4: You can not downgrade a custommer subscription with this function')
         
         else:
-            print(result) #The user already has the Subscription {current_plan}
+            print(result) #The user already has the Subscription {current_subscription}
             
     except:
         return Exception('Error code 3: other unknown error')
            
 
-def downgrade(id, new_plan):
+def downgrade(id, new_subscription):
+
+    """_summary_
+    The downgrade function allows to lower a customer's subscription, create or actualize
+    the date of the last downgrade and replaces the date of the last upgrade if it exists.
+    If the downgrade subscription is free turn all the ENABLED_FEATURES to False.
+    Downgrade function calls the up_or_down function which validates the new subscription.
+    
+    Args:
+        id ( UUID ): Unique code to identify each customer 
+        new_subscription ( Str ) : It is the new desired subscription, only allow free, basic or premium
+        
+    Raises:
+        Error Code 1: Bad id or does not exist.
+        Error Code 2: Bad subscription option or does not exist.
+        Error Code 3: (Exception) Occurs if something goes wrong with the microservice
+            for which the request is made or unkonw error.
+        Error Code 4: If you use the downgrade function to upgrade the subscription.
+        Error Code 5: The user already has the Subscription that you are trying to update.
+        
+    Returns:
+        Success ( Str ): Console message informing if the downgrade was successful.
+    """
 
     try:
         url = f'http://localhost:8010/api/v1/customerdata/{id}/'
@@ -60,17 +90,19 @@ def downgrade(id, new_plan):
             return print('Error code 1: the ID does not exist or is badly formated')
         
         data = customer['data']
-        current_plan = data["SUBSCRIPTION"]
-        result = up_or_down(current_plan, new_plan)
+        current_subscription = data["SUBSCRIPTION"]
+        result = up_or_down(current_subscription, new_subscription) # return True, False or a string message
         if result == False:
-            data['SUBSCRIPTION'] = new_plan
-            data['DOWNGRADE_DATE'] = datetime.now().isoformat()
+            data['SUBSCRIPTION'] = new_subscription
+            data['DOWNGRADE_DATE'] = datetime.now().isoformat() # .isoformat() make serializable the datetime
             try:
-                data.pop('UPGRADE_DATE')
+                data.pop('UPGRADE_DATE') # If UPGRADE_DATE exists then remove it, if not do nothing
             except:
                 pass
             
-            if new_plan == 'free':
+            # The for loop through each feature of ENABLED_FEATURES and, 
+            # for each iteration changes it to False
+            if new_subscription == 'free':
               for feature in data['ENABLED_FEATURES']:
                 data["ENABLED_FEATURES"][feature] = False
                        
@@ -82,24 +114,42 @@ def downgrade(id, new_plan):
             print('Error code 4: You can not upgrade a custommer subscription with this function')
         
         else:
-            print(result) #The user already has the Subscription {current_plan}
+            print(result) #The user already has the Subscription {current_subscription}
     
     except:
         raise Exception("Error code 3: other unknown error")
     
-def up_or_down(current_plan, new_plan): 
+def up_or_down(current_subscription, new_subscription): 
     
-    avaible_plans = ['free', 'basic', 'premium'] 
-    if new_plan not in avaible_plans:
+    """_summary_
+    The up_or_down function acts as an intermediary to validate that:
+    - The new subscription is valid and exists.
+    - The new subscription is not equal to the current subscription.
+    - It assigns numerical values to each type of subscription to confirm whether it has increased or decreased.
+    
+    Args:
+        current_subscription ( Str ): It is the current subscription of the customer .
+        new_subscription ( Str ) : It is the new desired subscription, only allow free, basic or premium.
+        
+    Returns:
+        The returns are inputs for the functions upgrade and downgrade.
+        True ( Boolean ) : If the new subscription is better than the current one.
+        False ( Boolean ) : If the new subscription is lower than the current one.
+        ( Str ): "Error code 2: the target subscription level is not reachable."
+        ( Str ): f"Error code 5: the user already has the Subscription {current_subscription}"  .   
+    """
+    
+    avaible_subscriptions = ['free', 'basic', 'premium'] 
+    if new_subscription not in avaible_subscriptions:
         return ('Error code 2: the target subscription level is not reachable') 
       
-    new_plan_value = avaible_plans.index(new_plan)
-    current_plan_value = avaible_plans.index(current_plan)
+    new_subscription_value = avaible_subscriptions.index(new_subscription)
+    current_subscription_value = avaible_subscriptions.index(current_subscription)
 
-    if new_plan_value == current_plan_value:
-        return (f'Error code 5: The user already has the Subscription {current_plan}')
-    elif new_plan_value > current_plan_value:
+    if new_subscription_value == current_subscription_value:
+        return (f'Error code 5: The user already has the Subscription {current_subscription}')
+    elif new_subscription_value > current_subscription_value:
         return True
-    elif new_plan_value < current_plan_value:
+    elif new_subscription_value < current_subscription_value:
         return False
-
+ 
