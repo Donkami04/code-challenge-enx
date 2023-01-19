85bf80d438e0701708a32e771cc219386d351f59
Donkami04 <camilo.munerac@gmail.com>
2023-01-19 11:01:30 -0500

    optimized code

--- 02_your_code/test/test_update_plan.py
+++ 02_your_code/test/test_update_plan.py
@@ -32,10 +32,10 @@ def test_downgrade_plan_customer():
 def is_the_plan_reachable(plan):
     avaible_plans = ['free', 'basic', 'premium']
     if plan not in avaible_plans:
-        raise SyntaxError('Error code 2: the target subscription level is not reachable')
+        raise ValueError('Error code 2: the target subscription level is not reachable')
     
 def test_test_if_plan_is_reachable():
-    with pytest.raises(SyntaxError):
+    with pytest.raises(ValueError):
         is_the_plan_reachable('otroplan')
         
 def test_id_does_not_exists():
@@ -58,5 +58,6 @@ def test_downgrade_to_free_customer_plan_enabled_features_are_false():
         customer['data']['ENABLED_FEATURES']['ENABLE_COURSE_DISCOVERY'] = False
         customer['data']['ENABLED_FEATURES']['ENABLE_DASHBOARD_SEARCH'] = False
         customer['data']['ENABLED_FEATURES']['INSTRUCTOR_BACKGROUND_TASKS'] = False
+        
     response = requests.put(url, json=customer)
     assert response.status_code == 200
---
+++ 02_your_code/update_plan.py
@@ -0,0 +1,98 @@
+"""
+'requests' allow make any HTTP requests, also is used for connect microservices
+"""
+import requests
+
+from datetime import datetime
+
+
+    
+def upgrade(id, new_plan):
+    """_summary_
+        This function is used to upgrade the subscription of one customer.
+        Has differents validations layers
+    Args:
+        id (_type_): _description_
+        plan (_type_): _description_
+    Raises:
+        SyntaxError: _description_
+    Returns:
+        _type_: _description_
+    """
+
+    try:
+        url = f'http://localhost:8010/api/v1/customerdata/{id}/'
+        response = requests.get(url)
+        customer = response.json()
+        if response.status_code == 404:
+            return print('Error code 1: the ID does not exist or is badly formated') 
+              
+        current_plan = customer["data"]["SUBSCRIPTION"]
+        result = up_or_down(current_plan, new_plan)
+        if result == True:
+            customer['data']['SUBSCRIPTION'] = new_plan
+            customer['data']['UPGRADE_DATE'] = datetime.now().isoformat()
+            put_request = requests.put(url, json=customer)
+            if put_request.status_code == 200:
+                print("Success!!")
+    
+        elif result == False:
+            print('You can not downgrade a custommer subscription with this function')
+        
+        else:
+            print(result) #The user already has the Subscription {current_plan}
+            
+    except:
+        return Exception('Error code 3: other unknown error')
+           
+
+def downgrade(id, new_plan):
+
+    try:
+        url = f'http://localhost:8010/api/v1/customerdata/{id}/'
+        response = requests.get(url)
+        customer = response.json()
+        if response.status_code == 404:
+            return print('Error code 1: the ID does not exist or is badly formated')
+        
+        current_plan = customer["data"]["SUBSCRIPTION"]
+        result = up_or_down(current_plan, new_plan)
+        if result == False:
+            customer['data']['SUBSCRIPTION'] = new_plan
+            customer['data']['DOWNGRADE_DATE'] = datetime.now().isoformat()
+            
+            if new_plan == 'free':
+              for feature in customer['data']['ENABLED_FEATURES']:
+                customer["data"]["ENABLED_FEATURES"][feature] = False
+                       
+            put_request = requests.put(url, json=customer)
+            if put_request.status_code == 200:
+                print("Success!!")
+                
+        elif result == True:
+            print('You can not upgrade a custommer subscription with this function')
+        
+        else:
+            print(result) #The user already has the Subscription {current_plan}
+    
+    except:
+        raise Exception("Error code 3: other unknown error")
+    
+def up_or_down(current_plan, new_plan): 
+    
+    avaible_plans = ['free', 'basic', 'premium'] 
+    if new_plan not in avaible_plans:
+        return ('Error code 2: the target subscription level is not reachable') 
+      
+    new_plan_value = avaible_plans.index(new_plan)
+    current_plan_value = avaible_plans.index(current_plan)
+
+    if new_plan_value == current_plan_value:
+        return (f'The user already has the Subscription {current_plan}')
+    elif new_plan_value > current_plan_value:
+        return True
+    elif new_plan_value < current_plan_value:
+        return False
+
+downgrade('e02deb01-d7a4-430c-9461-2674bc1e935a', 'free')
+#upgrade('e02deb01-d7a4-430c-9461-2674bc1e935a', 'premium')
